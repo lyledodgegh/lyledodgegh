ab9e12e9e488553efe7cb4366f29364c319bb86f
Lyle Dodge <16616380+lyledodgegh@users.noreply.github.com>
2024-10-05 21:29:31 -0400

    Update daily-cleanup.bat

--- daily-cleanup.bat
+++ daily-cleanup.bat
@@ -1,2 +1,7 @@
+rem cleanup of your machine on login each time
+rem do a cleanmgr /sageset first to select everything
+rem then create a shortcut in "%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+rem pointing to "cmd.exe /c curl https://raw.githubusercontent.com/lyledodgegh/lyledodgegh/refs/heads/main/daily-cleanup.bat | cmd"
+
 del /s /q %HOMEDRIVE%%HOMEPATH%\AppData\Local\Temp\*
 cleanmgr /sagerun
